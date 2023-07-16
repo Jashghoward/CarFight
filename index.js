fe69c896e25a9920a5db53262da1accee3a10792
Josh Howard <jashghoward@icloud.com>
2023-07-16 09:46:09 +0200

    index.js added

--- index.js
+++ index.js
@@ -34,4 +34,33 @@ createAutocomplete({
     document.querySelector('.tutorial').classList.add('is-hidden');
     onCarSelect(car, document.querySelector('#left-summary'), 'left');
   }
-})
+});
+createAutocomplete({
+  ...autoCompleteConfig,
+  root: document.querySelector('#right-autocomplete'),
+  onOptionSelect(car) {
+    document.querySelctor('.tutorial').classList.add('is-hidden');
+    onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
+  }
+});
+
+let leftCar;
+let rightCar;
+const inCarSelect = async (car, summaryElement, side) => {
+  const response = await axios.get('https://api.api-ninjas.com/v1/cars?model=', {
+    params: {
+      apikey: 'EtnvpekrLk4FHAqtSiwMFA==vBQ9W24GI94mYv2M',
+        i: car.make
+        // check on api for the correct keys you will need to make this work
+    }
+  });
+
+  summaryElement.innerHTML = carTemplate(response.data);
+
+  if (side === 'left') {
+    leftCar = response.data;
+  } else {
+    rightCar = response.data;
+  }
+  // if statement for left and right search bars
+}
