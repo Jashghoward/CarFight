35ff51ff917c083bb445483ea844a750160ca225
Josh Howard <jashghoward@icloud.com>
2023-07-16 10:14:59 +0200

    Utils.js added

--- index.js
+++ index.js
@@ -62,5 +62,75 @@ const inCarSelect = async (car, summaryElement, side) => {
   } else {
     rightCar = response.data;
   }
+
+  if (leftCar && rightCar) {
+    runComparison();
+  }
   // if statement for left and right search bars
 }
+  const runComparison = () => {
+    const leftSideStats = document.querySelectorAll('#left-summary . notification');
+    const rightSideStats = document.querySelectorAll('#right-summary . notification');
+
+    leftSideStats.forEach((leftStat, index) => {
+      const rightStat = rightSideStats[index];
+
+      const leftSideValue = parseInt(leftStat.dataset.value);
+      const rightSideValue = parseInt(rightStat.dataset.value);
+
+      if(rightSideValue > leftSideValue) {
+        leftStat.classList.remove('is-primary');
+        leftStat.classList.add('is-warning');
+      } else {
+        rightStat.classList.remove('is-primary');
+        rightStat.classList.add('is-warning');
+      }
+    });
+  };
+
+  const carTemplate = (carDetail) => {
+    const engine = parseInt(carDetail.cylinders.replace(/\$/g, '').replace(/,/g, ''));
+    const gears = parseInt(carDetail.transmission);
+    const fuel = parseInt(carDetail.fuel_type);
+    const driveTrain = parseInt(carDetail.drive);
+
+    return `
+    <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${carDetail.make}" />
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>  
+  <article data-value=${awards} class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article data-value=${dollars} class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article data-value=${metascore} class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article data-value=${imdbRating} class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article data-value=${imdbVotes} class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+  </article>
+    `
+  }
+
+
+
